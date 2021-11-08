3ed6ef1696c681d900385957f2e83b04a7c15f20
Leonard Laput <lorno.leonard@gmail.com>
2021-11-09 02:32:46 +0800

    test: add dummy properties

--- src/handlers/property/functions/get-properties/get-properties.spec.ts
+++ src/handlers/property/functions/get-properties/get-properties.spec.ts
@@ -1,5 +1,7 @@
 import type { APIGatewayProxyEvent } from 'aws-lambda'
+import faker from 'faker'
 
+import PropertyData from '@models/PropertyData'
 import mongoClient from '@services/mongo/client'
 import { getProperties } from './get-properties'
 
@@ -8,6 +10,51 @@ afterAll(async () => {
 })
 
 describe('GET /property', () => {
+  beforeAll(async () => {
+    await mongoClient.connect()
+
+    // Create properties
+    let i = 0
+    while (i < 10) {
+      const property = await new PropertyData()
+      property.propertyId = faker.datatype.number({ min: 100000, max: 999999 })
+      property.propertyName = faker.address.streetAddress()
+      property.income = {
+        January: faker.datatype.number(),
+        February: faker.datatype.number(),
+        March: faker.datatype.number(),
+        April: faker.datatype.number(),
+        May: faker.datatype.number(),
+        June: faker.datatype.number(),
+        July: faker.datatype.number(),
+        August: faker.datatype.number(),
+        September: faker.datatype.number(),
+        October: faker.datatype.number(),
+        November: faker.datatype.number(),
+        December: faker.datatype.number(),
+      }
+      property.expense = {
+        January: faker.datatype.number(),
+        February: faker.datatype.number(),
+        March: faker.datatype.number(),
+        April: faker.datatype.number(),
+        May: faker.datatype.number(),
+        June: faker.datatype.number(),
+        July: faker.datatype.number(),
+        August: faker.datatype.number(),
+        September: faker.datatype.number(),
+        October: faker.datatype.number(),
+        November: faker.datatype.number(),
+        December: faker.datatype.number(),
+      }
+      await property.save()
+
+      i++
+    }
+
+    await mongoClient.disconnect()
+  })
+
   it('it should get a list of properties', async () => {
     const event = {} as APIGatewayProxyEvent
 
@@ -15,6 +62,7 @@ describe('GET /property', () => {
     if (response) {
       const { body: jsonBody, statusCode } = response
       const body = JSON.parse(jsonBody)
+      console.log(body)
       expect(statusCode).toBe(200)
       expect(body?.message).toBe('Successfully retrieved properties')
     }
